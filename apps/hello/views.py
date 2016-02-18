a8ee59bf2e492ec63d6677a2b12fb5d16db5d88e
yevhen-m <yvhn.yvhn@gmail.com>
2016-02-18 22:49:47 +0200

    ticket:1 Pass context to my home view

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,5 +1,15 @@
+import datetime
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'hello/index.html')
+    context_dict = {
+        'profile': {
+            'name': 'Yevhen',
+            'surname': 'Malov',
+            'date_of_birth': datetime.date.today(),
+            'bio': 'My bio',
+            'contact': 'yvhn.yvhn@gmail.com'
+        }
+    }
+    return render(request, 'hello/index.html', context_dict)
