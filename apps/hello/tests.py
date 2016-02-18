3c4a0ca3843f75eef1702bcd71b3ceae5094fdfb
yevhen-m <yvhn.yvhn@gmail.com>
2016-02-18 22:51:56 +0200

    ticket:1 Add assertions that my home page contains my data to the test

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -12,3 +12,8 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('hello/index.html')
         self.assertIn('profile', response.context)
+
+        self.assertContains(response, 'Yevhen')
+        self.assertContains(response, 'Malov')
+        self.assertContains(response, 'My bio')
+        self.assertContains(response, 'yvhn.yvnn@gmail.com')
