9feafe27c06cde1b5cc32445f77cd068c10721df
yevhen-m <yvhn.yvhn@gmail.com>
2016-02-18 23:59:04 +0200

    ticket:1 Add assert that home view passes Profile to the context

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from .models import Profile
+
 
 class HomePageTest(TestCase):
 
@@ -12,6 +14,13 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('hello/index.html')
         self.assertIn('profile', response.context)
+
+        profile = Profile.objects.get(name='Yevhen')
+
+        c = response.context
+        self.assertIsInstance(c['profile'], Profile)
+        self.assertEqual(c['profile'].name, profile.name)
+        self.assertEqual(c['profile'].surname, profile.surname)
 
         self.assertContains(response, 'Yevhen')
         self.assertContains(response, 'Malov')
