c11ab0a40c6cb9d968ba34e52437a542b2aac593
yevhen-m <yvhn.yvhn@gmail.com>
2016-02-18 23:34:03 +0200

    ticket:1 Define Profile model, create migrations and init data fixture

---
+++ apps/hello/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Profile'
+        db.create_table(u'hello_profile', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('surname', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('date_of_birth', self.gf('django.db.models.fields.DateField')()),
+            ('bio', self.gf('django.db.models.fields.TextField')()),
+            ('contact', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+        ))
+        db.send_create_signal(u'hello', ['Profile'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Profile'
+        db.delete_table(u'hello_profile')
+
+
+    models = {
+        u'hello.profile': {
+            'Meta': {'object_name': 'Profile'},
+            'bio': ('django.db.models.fields.TextField', [], {}),
+            'contact': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'date_of_birth': ('django.db.models.fields.DateField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'surname': ('django.db.models.fields.CharField', [], {'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['hello']
---
+++ apps/hello/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    bio = models.TextField()
+    contact = models.EmailField()
