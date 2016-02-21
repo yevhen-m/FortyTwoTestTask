3d998f04b12bb99002bb721a276115e9dfb7627d
yevhen-m <yvhn.yvhn@gmail.com>
2016-02-22 00:43:12 +0200

    ticket:5 Insert recieved errors into the form page

--- assets/js/edit_form.js
+++ assets/js/edit_form.js
@@ -4,15 +4,25 @@ $(document).ready(function() {
     $('#editForm').ajaxForm(
         {
             beforeSubmit: function() {
-                // disable inputs
+                $('.error').remove();
             },
             success: function() {
                 $st.show();
                 $st.text('Successfully changed!');
             },
-            error: function() {
+            error: function(response) {
+                var errors = JSON.parse(response.responseText);
+                console.log(errors);
                 $st.show();
                 $st.text('There was an ERROR!');
+                for (var input_name in errors) {
+                    var $input = $('#id_' + input_name);
+
+                    var input_errors = errors[input_name];
+                    for (var i=0; i < input_errors.length; i++) {
+                        $input.closest('div').prepend('<p class="error">' + input_errors[i] + '</p>');
+                    }
+                }
             }
         }
     );
