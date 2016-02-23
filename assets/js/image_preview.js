5c0e0c151b71288b81a52edff0011fb4f3ae9c6f
yevhen-m <yvhn.yvhn@gmail.com>
2016-02-23 19:11:04 +0200

    ticket:5 Add js to preview chosen img on the edit profile page

---
+++ assets/js/image_preview.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    function readURL(input) {
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#img_preview').attr('src', e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#id_photo").change(function(){
+        readURL(this);
+    });
+});
