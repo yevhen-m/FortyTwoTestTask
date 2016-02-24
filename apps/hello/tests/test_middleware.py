76b659255c8db4e3a090725eaf9d315f55cd0c55
yevhen-m <yvhn.yvhn@gmail.com>
2016-02-25 01:01:12 +0200

    ticket:2 Test that middleware does not store ajax request with ?store=false

--- apps/hello/tests/test_middleware.py
+++ apps/hello/tests/test_middleware.py
@@ -29,3 +29,31 @@ class RequestsMiddlewareTest(TestCase):
         requests = Request.objects.all()
 
         self.assertEqual(requests.count(), 5)
+
+    def test_middleware_does_not_save_ajax_requests(self):
+        '''
+        Test that middleware ignores ajax requests with query string
+        `?store=false`.
+        '''
+        # Request model is ordered my timestamp descending
+        for _ in xrange(12):
+            Request.objects.create(
+                method='GET',
+                path='/',
+                query=''
+            )
+
+        newest_request_id_before_ajax_request = Request.objects.first().id
+
+        self.client.get(
+            self.url,
+            {'store': 'false'},
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest'
+        )
+
+        newest_request_id_after_ajax_request = Request.objects.first().id
+
+        self.assertEqual(
+            newest_request_id_before_ajax_request,
+            newest_request_id_after_ajax_request
+        )
