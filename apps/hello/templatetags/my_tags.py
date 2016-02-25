3c80868f3f562d2d3280cb0300b9d99ff4551fcb
yevhen-m <yvhn.yvhn@gmail.com>
2016-02-25 22:44:59 +0200

    ticket:8 Add my admin_url_link tag

---
+++ apps/hello/templatetags/my_tags.py
@@ -0,0 +1,15 @@
+from django import template
+from django.core.urlresolvers import reverse
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('hello/admin_link_url.html')
+def admin_link_url(obj):
+    admin_view_name = 'admin:{}_{}_change'.format(
+        obj._meta.app_label,
+        obj._meta.model_name
+    )
+    obj_admin_url = reverse(admin_view_name, args=(obj.pk,))
+    return dict(object_admin_url=obj_admin_url)
