0648c15486b883885e198c9015ac12075aa5d3b6
yevhen-m <yvhn.yvhn@gmail.com>
2016-02-27 15:33:54 +0200

    ticket:8 Refactor my test_signals.py module

--- apps/hello/signals/test_signals.py
+++ apps/hello/signals/test_signals.py
@@ -7,24 +7,29 @@ from apps.hello.models import Request, DBAction
 
 class SignalTest(TestCase):
 
-    def test_db_action_signal_handler(self):
-        '''
-        Test that my signal handlers stores objects creation to db.
-        '''
-        timestamp = datetime.datetime.now()
-
-        request = Request.objects.create(
+    def setUp(self):
+        Request.objects.create(
             method='GET',
             path='/',
             query='',
-            timestamp=timestamp
+            timestamp=datetime.datetime.now()
         )
 
+    def test_db_action_signal_handler_deals_with_objects_creation(self):
+        '''
+        Test that my signal handler stores data about objects creation
+        to db.
+        '''
         db_action = DBAction.objects.last()
 
         self.assertEqual(db_action.model, 'Request')
         self.assertEqual(db_action.action, 'created')
 
+    def test_db_action_signal_handler_deals_with_objects_editing(self):
+        '''
+        Test that my handler handles objects editing.
+        '''
+        request = Request.objects.last()
         request.method = 'POST'
         request.save()
 
@@ -32,3 +37,15 @@ class SignalTest(TestCase):
 
         self.assertEqual(db_action.model, 'Request')
         self.assertEqual(db_action.action, 'updated')
+
+    def test_db_action_signal_handler_deals_with_object_deletion(self):
+        '''
+        Test that my handler handles post_delete signal.
+        '''
+        request = Request.objects.first()
+        request.delete()
+
+        db_action = DBAction.objects.last()
+
+        self.assertEqual(db_action.model, 'Request')
+        self.assertEqual(db_action.action, 'deleted')
