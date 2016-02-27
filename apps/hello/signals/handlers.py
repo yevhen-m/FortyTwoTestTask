e0f8e7948d6dec06e9df087eaf08330bf1b688ba
yevhen-m <yvhn.yvhn@gmail.com>
2016-02-27 15:47:43 +0200

    ticket:8 Add post_delete signal handler

--- apps/hello/signals/handlers.py
+++ apps/hello/signals/handlers.py
@@ -1,11 +1,11 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from apps.hello.models import DBAction
 
 
 @receiver(post_save)  # noqa
-def object_created(sender, created, **kwargs):
+def object_created_or_updated(sender, created, **kwargs):
     if created and sender is not DBAction:
         DBAction.objects.create(
             model=sender.__name__,
@@ -15,4 +15,13 @@ def object_created(sender, created, **kwargs):
         DBAction.objects.create(
             model=sender.__name__,
             action='updated'
+        )
+
+
+@receiver(post_delete)  # noqa
+def object_deleted(sender, **kwargs):
+    if sender is not DBAction:
+        DBAction.objects.create(
+            model=sender.__name__,
+            action='deleted'
         )
