7a2cc132c84adb607270f9c42747e8f6ec3b747a
yevhen-m <yvhn.yvhn@gmail.com>
2016-03-04 21:56:36 +0200

    ticket:8 Refactor signals handlers

--- apps/hello/signals/handlers.py
+++ apps/hello/signals/handlers.py
@@ -9,33 +9,27 @@ from apps.hello.models import DBAction
 IGNORED_SENDERS = (DBAction, LogEntry)
 
 
+def create_DBAction(sender, action):
+    try:
+        DBAction.objects.create(
+            model=sender.__name__,
+            action=action
+        )
+    except OperationalError:
+        pass
+
+
 @receiver(post_save)  # noqa
 def object_created_or_updated(sender, created, **kwargs):
-    if created and sender not in IGNORED_SENDERS:
-        try:
-            DBAction.objects.create(
-                model=sender.__name__,
-                action='created'
-            )
-        except OperationalError:
-            pass
-    elif sender not in IGNORED_SENDERS:
-        try:
-            DBAction.objects.create(
-                model=sender.__name__,
-                action='updated'
-            )
-        except OperationalError:
-            pass
+    if sender in IGNORED_SENDERS:
+        return
+
+    create_DBAction(sender, action=('created' if created else 'updated'))
 
 
 @receiver(post_delete)  # noqa
 def object_deleted(sender, **kwargs):
-    if sender not in IGNORED_SENDERS:
-        try:
-            DBAction.objects.create(
-                model=sender.__name__,
-                action='deleted'
-            )
-        except OperationalError:
-            pass
+    if sender in IGNORED_SENDERS:
+        return
+
+    create_DBAction(sender, 'deleted')
